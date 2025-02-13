b257188f0272bf8a5824ff4cf236b6af04616d7e
yonghun <asqwklop12@gmail.com>
2025-02-13 12:44:37 +0900

    git init

---
+++ src/main/java/com/sparta/bedelivery/BeDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.bedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/bedelivery/BeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
