0e133932ce9b941e280bc144db2852cf904f77d8
rana0122 <rana0122@naver.com>
2025-02-18 19:16:47 +0900

    user  기능 구현 - 반환타입 수정

--- src/main/java/com/sparta/bedelivery/controller/UserAddressController.java
+++ src/main/java/com/sparta/bedelivery/controller/UserAddressController.java
@@ -1,6 +1,7 @@
 package com.sparta.bedelivery.controller;
 
 import com.sparta.bedelivery.dto.*;
+import com.sparta.bedelivery.global.response.ApiResponseData;
 import com.sparta.bedelivery.service.UserAddressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,34 +21,34 @@ public class UserAddressController {
 
     //2.1 배송지 추가
     @PostMapping
-    public ResponseEntity<UserAddressResponse> addUserAddress(@AuthenticationPrincipal UserDetails userDetails,
-                                                              @RequestBody UserAddressRequest request) {
-        return ResponseEntity.ok(userAddressService.addUserAddress(userDetails.getUsername(), request));
+    public ResponseEntity<ApiResponseData<UserAddressResponse>> addUserAddress(@AuthenticationPrincipal UserDetails userDetails,
+                                                                               @RequestBody UserAddressRequest request) {
+        return ResponseEntity.ok(ApiResponseData.success(userAddressService.addUserAddress(userDetails.getUsername(), request)));
     }
 
     //2.2 배송지 목록 조회
     @GetMapping
-    public ResponseEntity<List<UserAddressResponse>> getUserAddresses(@AuthenticationPrincipal UserDetails userDetails) {
-        return ResponseEntity.ok(userAddressService.getUserAddresses(userDetails.getUsername()));
+    public ResponseEntity<ApiResponseData<List<UserAddressResponse>>> getUserAddresses(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(ApiResponseData.success(userAddressService.getUserAddresses(userDetails.getUsername())));
     }
 
     //2.3 배송지 수정
     @PutMapping("/{addressId}")
-    public ResponseEntity<UserAddressResponse> updateUserAddress(@PathVariable UUID addressId,
-                                                                 @RequestBody UserAddressRequest request) {
-        return ResponseEntity.ok(userAddressService.updateUserAddress(addressId, request));
+    public ResponseEntity<ApiResponseData<UserAddressResponse>> updateUserAddress(@PathVariable UUID addressId,
+                                                                                  @RequestBody UserAddressRequest request) {
+        return ResponseEntity.ok(ApiResponseData.success(userAddressService.updateUserAddress(addressId, request)));
     }
 
     //2.4 배송지 삭제
     @DeleteMapping("/{addressId}")
-    public ResponseEntity<?> deleteUserAddress(@PathVariable UUID  addressId) {
+    public ResponseEntity<ApiResponseData<String>> deleteUserAddress(@PathVariable UUID  addressId) {
         userAddressService.deleteUserAddress(addressId);
-        return ResponseEntity.ok("{\"message\": \"배송지가 삭제되었습니다.\"}");
+        return ResponseEntity.ok(ApiResponseData.success("배송지가 삭제되었습니다."));
     }
 
     //2.5 배송지 단건 조회
     @GetMapping("/{addressId}")
-    public ResponseEntity<UserAddressResponse> getUserAddress(@PathVariable UUID  addressId) {
-        return ResponseEntity.ok(userAddressService.getUserAddress(addressId));
+    public ResponseEntity<ApiResponseData<UserAddressResponse>> getUserAddress(@PathVariable UUID  addressId) {
+        return ResponseEntity.ok(ApiResponseData.success(userAddressService.getUserAddress(addressId)));
     }
 }
--- src/main/java/com/sparta/bedelivery/controller/UserController.java
+++ src/main/java/com/sparta/bedelivery/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.sparta.bedelivery.controller;
 
 import com.sparta.bedelivery.dto.*;
+import com.sparta.bedelivery.global.response.ApiResponseData;
 import com.sparta.bedelivery.security.CustomUserDetails;
 import com.sparta.bedelivery.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -19,23 +20,22 @@ public class UserController {
 
     // 1.4 내 정보 조회
     @GetMapping("/me")
-    public ResponseEntity<UserResponse> getUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        return ResponseEntity.ok(userService.getUserInfo(userDetails.getUsername()));
+    public ResponseEntity<ApiResponseData<UserResponse>> getUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(ApiResponseData.success(userService.getUserInfo(userDetails.getUsername())));
     }
 
     // 1.5 내 정보 수정
     @PutMapping("/me")
-    public ResponseEntity<UserResponse> updateUser(@AuthenticationPrincipal CustomUserDetails userDetails,
-                                                   @RequestBody UserUpdateRequest request) {
-//        System.out.println("User Authorities: " + userDetails.getAuthorities());
-        return ResponseEntity.ok(userService.updateUser(userDetails.getUsername(), request));
+    public ResponseEntity<ApiResponseData<UserResponse>> updateUser(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                                    @RequestBody UserUpdateRequest request) {
+        return ResponseEntity.ok(ApiResponseData.success(userService.updateUser(userDetails.getUsername(), request)));
     }
 
     // 1.6 사용자 탈퇴
     @DeleteMapping("/me")
-    public ResponseEntity<?> deleteUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ResponseEntity<ApiResponseData<String>> deleteUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
         userService.deleteUser(userDetails.getUsername());
-        return ResponseEntity.ok(userDetails.getUsername()+" 계정이 삭제되었습니다.");
+        return ResponseEntity.ok(ApiResponseData.success(userDetails.getUsername() + " 계정이 삭제되었습니다."));
     }
 
 }
