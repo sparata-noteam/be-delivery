52e9dae50412894400c62c06df2d661f40939e37
rana0122 <rana0122@naver.com>
2025-02-19 11:17:49 +0900

    회원가입시 create,update 모두 들어가는 현상수정 -UserAuditorAware 걷어냄 -비효율적이라 판단 -회원가입시 dto에서 입력받은 값으로 createby를 해줘야 하는데, UserAuditorAware를 사용하여 구현하면 특수 케이스가 전역에 걸쳐 영향도가 높아진다 판단하여 제거

--- src/main/java/com/sparta/bedelivery/entity/User.java
+++ src/main/java/com/sparta/bedelivery/entity/User.java
@@ -55,6 +55,7 @@ public class User extends BaseSystemFieldEntity {
         this.name = request.getName();
         this.phone = request.getPhone();
         this.role = Role.fromString(null);
+        this.setCreateBy(request.getUserId());
     }
 
     public User(String userId) {
