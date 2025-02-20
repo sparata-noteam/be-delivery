f62f945046734df1f16d790a4662df248f93b3b4
Your Name <kim946509@gmail.com>
2025-02-20 11:26:26 +0900

    fix : QueryDsl 중복 삭제

--- src/main/java/com/sparta/bedelivery/config/QueryDslConfig.java
+++ src/main/java/com/sparta/bedelivery/config/QueryDslConfig.java
@@ -4,13 +4,18 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.query.JpaQueryCreator;
 
 @Configuration
-public class QueryDslConfig {
-    @Bean
-    JPAQueryFactory jpaQueryFactory(EntityManager em) {
-        return new JPAQueryFactory(em);
+public class QuerydslConfig {
+
+    private final EntityManager entityManager;
+
+    public QuerydslConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 }
